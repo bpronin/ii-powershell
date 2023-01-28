961f67f2b042809d2109d29f8656816505afe477
Bo <boprsoft.dev@gmail.com>
2023-01-28 20:22:06 +0300

    add powershell version detection

--- src/main/java/com/bopr/intellij/iipowershell/run/PowerShellRunConfigurationEditor.java
+++ src/main/java/com/bopr/intellij/iipowershell/run/PowerShellRunConfigurationEditor.java
@@ -5,38 +5,53 @@ import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.options.SettingsEditor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
-import com.intellij.ui.components.fields.ExtendableTextField;
+import com.intellij.ui.DocumentAdapter;
+import com.intellij.ui.components.fields.ExpandableTextField;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
 import java.nio.file.Path;
 
 import static com.bopr.intellij.iipowershell.language.PowerShellFileType.POWERSHELL_FILE_EXTENSION;
 import static com.bopr.intellij.iipowershell.language.Resources.string;
+import static com.bopr.intellij.iipowershell.util.IoUtilKt.detectPowerShellVersion;
+import static java.lang.String.format;
 
 /* NOTE: Do not convert into Kotlin. Form designer does not work well with it. */
 public class PowerShellRunConfigurationEditor extends SettingsEditor<PowerShellRunConfiguration> {
 
     private JPanel editorPanel;
     private TextFieldWithBrowseButton scriptEditor;
-    private ExtendableTextField argumentsEditor;
-    private TextFieldWithBrowseButton executableEditor;
+    private ExpandableTextField argumentsEditor;
+    private TextFieldWithBrowseButton interpreterEditor;
     private TextFieldWithBrowseButton workingDirectoryEditor;
     private EnvironmentVariablesTextFieldWithBrowseButton environmentVariablesEditor;
+    private JLabel interpreterVersionLabel;
 
     public PowerShellRunConfigurationEditor(Project project) {
+        interpreterEditor.addBrowseFolderListener(string("interpreter"), string("choose_powershell_executable"), project,
+                new FileChooserDescriptor(true, false, false, false, false, false)
+                        .withFileFilter(virtualFile -> "exe".equals(virtualFile.getExtension()))
+        );
+        interpreterEditor.getTextField().getDocument().addDocumentListener(new DocumentAdapter() {
+
+            @Override
+            protected void textChanged(@NotNull DocumentEvent e) {
+                updateVersionLabel();
+            }
+        });
+
         scriptEditor.addBrowseFolderListener(string("script"), string("choose_script"), project,
                 new FileChooserDescriptor(true, false, false, false, false, false)
                         .withFileFilter(virtualFile -> POWERSHELL_FILE_EXTENSION.equals(virtualFile.getExtension()))
         );
-        executableEditor.addBrowseFolderListener(string("interpreter"), string("choose_powershell_executable"), project,
-                new FileChooserDescriptor(true, false, false, false, false, false)
-                        .withFileFilter(virtualFile -> "exe".equals(virtualFile.getExtension()))
-        );
+
         workingDirectoryEditor.addBrowseFolderListener(string("working_directory"), string("choose_script_working_directory"), project,
                 new FileChooserDescriptor(false, true, false, false, false, false)
         );
 
+        updateVersionLabel();
 //        MacrosDialog.addMacroSupport(workingDirectoryEditor, MacrosDialog.Filters.ALL, () -> false);
     }
 
@@ -49,7 +64,7 @@ public class PowerShellRunConfigurationEditor extends SettingsEditor<PowerShellR
     protected void resetEditorFrom(@NotNull PowerShellRunConfiguration configuration) {
         scriptEditor.setText(configuration.getScriptPath().toString());
         argumentsEditor.setText(configuration.getScriptArguments());
-        executableEditor.setText(configuration.getInterpreterPath().toString());
+        interpreterEditor.setText(configuration.getInterpreterPath().toString());
         workingDirectoryEditor.setText(configuration.getWorkingDirectory().toString());
         environmentVariablesEditor.setData(configuration.getEnvironmentVariables());
     }
@@ -58,9 +73,15 @@ public class PowerShellRunConfigurationEditor extends SettingsEditor<PowerShellR
     protected void applyEditorTo(@NotNull PowerShellRunConfiguration configuration) /*throws ConfigurationException*/ {
         configuration.setScriptPath(Path.of(scriptEditor.getText()));
         configuration.setScriptArguments(argumentsEditor.getText());
-        configuration.setInterpreterPath(Path.of(executableEditor.getText()));
+        configuration.setInterpreterPath(Path.of(interpreterEditor.getText()));
         configuration.setWorkingDirectory(Path.of(workingDirectoryEditor.getText()));
         configuration.setEnvironmentVariables(environmentVariablesEditor.getData());
+    }
+
+    private void updateVersionLabel() {
+        String version = detectPowerShellVersion(Path.of(interpreterEditor.getText()));
+        String text = (version != null) ? format(string("powershell_version"), version) : string("not_a_powershell");
+        interpreterVersionLabel.setText(text);
     }
 
 }
--- src/main/kotlin/com/bopr/intellij/iipowershell/util/IoUtil.kt
+++ src/main/kotlin/com/bopr/intellij/iipowershell/util/IoUtil.kt
@@ -2,7 +2,9 @@ package com.bopr.intellij.iipowershell.util
 
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.util.io.exists
+import java.io.BufferedReader
 import java.nio.file.Path
+import java.util.concurrent.TimeUnit
 
 fun findAbsolutePath(path: Path): Path {
     if (path.exists()) return path
@@ -14,4 +16,27 @@ fun findAbsolutePath(path: Path): Path {
     }
 
     return path
+}
+
+fun detectPowerShellVersion(path: Path): String? {
+    val signature = "i-am-really-the-powershell"
+    val process = try {
+        ProcessBuilder(
+            path.toString(), "-Command",
+            "Write-Host('$signature');", "Write-Host(\$PSVersionTable.PSVersion);"
+        )
+            .redirectErrorStream(true)
+            .start()
+    } catch (e: Exception) {
+        return null
+    }
+
+    val reader = BufferedReader(process.inputStream.reader())
+    try {
+        if (signature != reader.readLine()) return null
+        return reader.readLine()
+    } finally {
+        reader.close()
+        process.waitFor(3, TimeUnit.SECONDS)
+    }
 }
