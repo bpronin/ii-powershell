9ef5f6e9a9bf7daaf94c17bfd5394d5d9cfbb291
Bo <boprsoft.dev@gmail.com>
2023-01-29 18:46:25 +0300

    add grammar rules for delimited comment

--- src/main/resources/values/powershell-demo-text.ps1
+++ src/main/resources/values/powershell-demo-text.ps1
@@ -1,3 +1,36 @@
+# Single line comment
+
+<########################
+#Delimited comment  #
+
+
+
+ #########################>
+
+$include = @("*.mkv", "*.mp4")
+
+Import-Module -Name $PSScriptRoot\ffmpeg
+
+class Track
+{
+    [Int]$Index
+}
+
+function Read-Time
+{
+    param (
+        [String]$String
+    )
+    process {
+        $timeString = $String.Trim()
+        if ($timeString.split(":").count -eq 2)
+        {
+            $timeString = "00:$timeString"
+        }
+        return [TimeSpan]$timeString
+    }
+}
+
 # SIG # Begin signature block
 # MIInzAYJKoZIhvcNAQcCoIInvTCCJ7kCAQExDzANBglghkgBZQMEAgEFADB5Bgor
 # ...
