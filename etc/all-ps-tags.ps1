671678366192d953bdf62e44f5661cf98bf9dbec
Bo <boprsoft.dev@gmail.com>
2023-01-30 14:40:28 +0300

    add grammar rules for real literals

--- etc/all-ps-tags.ps1
+++ etc/all-ps-tags.ps1
@@ -1,13 +1,23 @@
 # Single line comment
 #
 
+#Requires something
+
 <########################
 # Delimited comment     #
 #########################>
 
-$$ $? $^ $variable $variable? ${ this is also `variable?` }
+# Variables
+$$ $? $^ $_ $variable $global:variable $namespace:variable $variable? ${ this is also a `variable?` } @global:variable
 
-if ($token) return $token
+# Integer literals
+123 10 123L 10kb 123LPB 0x12FFF +10 -20
+
+# Real literals
+123.55 .58  1.2e5  1.2e5l 1.2E5l 1.2e5dKB 1e23 2e5dKB  +123.55 -123.55
+
+if $token return $token
+
 
 # SIG # Begin signature block
 # MIInzAYJKoZIhvcNAQcCoIInvTCCJ7kCAQExDzANBglghkgBZQMEAgEFADB5Bgor
