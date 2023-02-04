508a07a9ca89d47b77767d7ad374192bcb1b6d50
Bo <boprsoft.dev@gmail.com>
2023-02-04 16:46:07 +0300

    add psi utils

---
+++ src/main/kotlin/com/bopr/intellij/iipowershell/language/psi/PowerShellPsiUtil.kt
@@ -0,0 +1,26 @@
+package com.bopr.intellij.iipowershell.language.psi
+
+object PowerShellPsiUtil {
+
+    /* see https://plugins.jetbrains.com/docs/intellij/psi-helper-and-utilities.html#update-grammar-and-regenerate-the-parser */
+
+//    fun getKey(element: SimpleProperty): String? {
+//        val keyNode: ASTNode = element.getNode().findChildByType(SimpleTypes.KEY)
+//        return if (keyNode != null) {
+//            // IMPORTANT: Convert embedded escaped spaces to simple spaces
+//            keyNode.text.replace("\\\\ ".toRegex(), " ")
+//        } else {
+//            null
+//        }
+//    }
+//
+//    fun getValue(element: SimpleProperty): String? {
+//        val valueNode: ASTNode = element.getNode().findChildByType(SimpleTypes.VALUE)
+//        return if (valueNode != null) {
+//            valueNode.text
+//        } else {
+//            null
+//        }
+//    }
+
+}
